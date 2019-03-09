ef508d3f09114745b876dcec0fa8b3aad9796a7d
stephsadev <stephaniespringart@gmail.com>
2019-03-08 19:24:42 -0500

    First commit

---
+++ Exercise-2/Exercise 2/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        value: '',
+        test: 'test'
+    },
+    methods: {
+      alertMe: function(){
+        alert("You clicked me!");
+      }
+    }
+});
